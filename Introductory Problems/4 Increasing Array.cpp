70d9f315545d2403f7be1c81f320e6d2910af55b
tanishq8000 <108824230+tanishq8000@users.noreply.github.com>
2024-10-01 12:31:36 +0530

    Add files via upload

---
+++ Introductory Problems/4 Increasing Array.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+
+int32_t main() {
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++)
+	{
+	    cin>>arr[i];
+	}
+	
+	int moves = 0;
+	
+	for(int i=1;i<n;i++)
+	{
+	    if(arr[i] < arr[i-1])
+	    {
+	        moves += (arr[i-1] - arr[i]);
+	        arr[i] = arr[i-1];
+	    }
+	}
+	
+	cout<<moves<<endl;
+	return 0;
+}
