852bcda681b4b2fbf72620c9068f304bb4f3132f
tanishq8000 <108824230+tanishq8000@users.noreply.github.com>
2024-11-15 11:58:35 +0530

    Add files via upload

---
+++ Sorting and Searching/28. Subarray Sums-2.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+ 
+int32_t main() {
+    int n,x;
+    cin>>n>>x;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    
+    map<int,int> mp;
+    mp[0] = 1;
+    int ans = 0;
+    int currSum = 0;
+    
+    for(int i=0;i<n;i++)
+    {
+        currSum += arr[i];
+        int req = currSum-x;
+        
+        if(mp.find(req) != mp.end())
+        {
+            ans += (mp[req]);
+        }
+        mp[currSum]++;
+    }
+    
+    cout<<ans<<endl;
+    return 0;
+}
